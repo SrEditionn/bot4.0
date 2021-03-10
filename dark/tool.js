411d596c8120d21049083487eb8365156e4e009d
SrEditionn <71359788+SrEditionn@users.noreply.github.com>
2021-03-09 23:42:20 -0300

    Update tool.js

--- dark/tool.js
+++ dark/tool.js
@@ -3,7 +3,7 @@ const tool = () => {
 ──────────────────
 *TOOL HACKER TERMUX*
 ──────────────────
-        『 𝕯𝖆𝖗𝖐 𝕭𝖔𝖙 』
+        『 𝕭𝖗𝖎𝖟𝖆𝖘-𝖇𝖔𝖙 』
 ──────────────────
 apt-get install python3
 
@@ -17,7 +17,7 @@ cd Cam-Hackers
 
 python3 cam-hackers.py
 ──────────────────
-        『 𝕯𝖆𝖗𝖐 𝕭𝖔𝖙 』
+        『 𝕭𝖗𝖎𝖟𝖆𝖘-𝖇𝖔𝖙 』
 ──────────────────`
 }
 exports.tool = tool
